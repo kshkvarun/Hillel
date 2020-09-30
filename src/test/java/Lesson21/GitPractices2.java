10d3684f9f4b33670b7ead4851e6c4d6930a4968
k.shkvarun <k0styan19894@gmail.com>
2020-09-30 22:51:35 +0300

    Add delete

--- src/test/java/Lesson21/GitPractices2.java
+++ src/test/java/Lesson21/GitPractices2.java
@@ -1,6 +1,7 @@
 package Lesson21;public class GitPractices2 {
     public static void main(String[] args) {
         System.out.println("new branch");
+        System.out.println("say by");
     }
 
 }
