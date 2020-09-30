4000db626cf25b37fb483543de3027097d68d2b0
k.shkvarun <k0styan19894@gmail.com>
2020-09-30 22:43:16 +0300

    Add delete

--- src/test/java/Lesson21/GitPractices3.java
+++ src/test/java/Lesson21/GitPractices3.java
@@ -3,5 +3,7 @@ package Lesson21;
 public class GitPractices3 {
     public static void main(String[] args) {
         System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
