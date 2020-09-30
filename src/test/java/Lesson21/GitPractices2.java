139a4b6d6604d1740f81d0504e382d83ec820372
k.shkvarun <k0styan19894@gmail.com>
2020-09-30 23:00:16 +0300

    New branch

--- src/test/java/Lesson21/GitPractices2.java
+++ src/test/java/Lesson21/GitPractices2.java
@@ -2,6 +2,7 @@ package Lesson21;public class GitPractices2 {
     public static void main(String[] args) {
         System.out.println("new branch");
         System.out.println("say by");
+        System.out.println("using new branch");
     }
 
 }
