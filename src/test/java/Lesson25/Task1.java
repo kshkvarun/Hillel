b4e130bc3b348789184afd2156ada898e7adef22
k.shkvarun <k0styan19894@gmail.com>
2020-10-17 15:13:22 +0300

    Add lesson 25

--- src/test/java/Lesson25/Task1.java
+++ src/test/java/Lesson25/Task1.java
@@ -18,12 +18,22 @@ public class Task1 extends BaseClass {
     private String rozetkaUrl = "https://rozetka.com.ua/";
     String itemComputersAndLaptops = "//sidebar-fat-menu//a[contains(text(), 'Ноутбуки и компьютеры')]";
     String itemMonitors = "//fat-menu/div/ul/li[1]/div/div[2]/div[1]/div[2]/ul[2]/li/ul/li[1]";
-
-    //    String itemMonitors = "//a[contains(text(),'Мониторы')]";
+//    Лично у меня не хотело искать по этим локаторам, у гих они работали, поэтому так коряво сделал
+//    String itemMonitors = "//a[contains(text(),'Мониторы')]";
 //    String itemMonitors1 = "//fat-menu//a[[text()=' Мониторы ']";
     String goodPicture = "a.goods-tile__picture";
     String prices = "span.goods-tile__price-value";
     String products = "span.goods-tile__title";
+    String addToCompare = "button.compare-button";
+    String productCompareCounter = "//span[@class='header-actions__button-counter']";
+    String productTitleInCompare = "a.product__heading";
+    String compareProductsButton = "button.header-actions__button";
+    String compareLink = "a.comparison-modal__link";
+
+
+
+
+
 //  String menuCatalog = "//a[contains(text(), 'Ноутбуки и компьютеры')]";
 
     @BeforeMethod
@@ -57,18 +67,17 @@ public class Task1 extends BaseClass {
             }
         }
 
-        System.out.println(firstPrice);
-
 
         List<WebElement> productList = driver.findElements(By.cssSelector(products));
         System.out.println(firstPriceIndex);
         String firstProductName = productList.get(firstPriceIndex).getText();
         System.out.println(firstProductName);
 
-        driver.findElement(By.xpath("//span[@class='goods-tile__title'] [contains(text(), '" + firstProductName + "')]")).click();
-        wait.until(visibilityOfElementLocated(By.cssSelector("button.compare-button"))).click();
-        wait.until(visibilityOfElementLocated(By.xpath("//span[@class='header-actions__button-counter']")));
-        Assert.assertTrue(driver.findElement(By.xpath("//span[@class='header-actions__button-counter']")).getText().contains("1"));
+        String productTitle = "//span[@class='goods-tile__title'] [contains(text(), '" + firstProductName + "')]";
+        driver.findElement(By.xpath(productTitle)).click();
+        wait.until(visibilityOfElementLocated(By.cssSelector(addToCompare))).click();
+        wait.until(visibilityOfElementLocated(By.xpath(productCompareCounter)));
+        Assert.assertTrue(driver.findElement(By.xpath(productCompareCounter)).getText().contains("1"));
         driver.navigate().back();
         wait.until(visibilityOfElementLocated(By.cssSelector(goodPicture)));
 
@@ -97,13 +106,13 @@ public class Task1 extends BaseClass {
         System.out.println(secondProductName);
 
         driver.findElement(By.xpath("//span[@class='goods-tile__title'] [contains(text(), '" + secondProductName + "')]")).click();
-        wait.until(visibilityOfElementLocated(By.cssSelector("button.compare-button"))).click();
-        wait.until(textToBe(By.xpath("//span[@class='header-actions__button-counter']"), "2"));
+        wait.until(visibilityOfElementLocated(By.cssSelector(addToCompare))).click();
+        wait.until(textToBe(By.xpath(productCompareCounter), "2"));
 
-        Assert.assertTrue(driver.findElement(By.xpath("//span[@class='header-actions__button-counter']")).getText().contains("2"));
+        Assert.assertTrue(driver.findElement(By.xpath(productCompareCounter)).getText().contains("2"));
 
-        driver.findElement(By.cssSelector("button.header-actions__button")).click();
-        driver.findElement(By.cssSelector("a.comparison-modal__link")).click();
+        driver.findElement(By.cssSelector(compareProductsButton)).click();
+        driver.findElement(By.cssSelector(compareLink)).click();
         wait.until(visibilityOfElementLocated(By.cssSelector("h1.comparison__heading")));
 //        sleep(5000);
 
@@ -112,8 +121,8 @@ public class Task1 extends BaseClass {
         assertThat(2, equalTo(compareList.size()));
 
         List<WebElement> checkList = driver.findElements(By.cssSelector("div.product"));
-        assertThat(checkList.get(0).findElement(By.cssSelector("a.product__heading")).getText(), equalTo(firstProductName));
-        assertThat(checkList.get(1).findElement(By.cssSelector("a.product__heading")).getText(), equalTo(secondProductName));
+        assertThat(checkList.get(0).findElement(By.cssSelector(productTitleInCompare)).getText(), equalTo(firstProductName));
+        assertThat(checkList.get(1).findElement(By.cssSelector(productTitleInCompare)).getText(), equalTo(secondProductName));
 
         String firstPriceInCompare = checkList.get(0).findElement(By.cssSelector("div.product__price.product__price--red")).getText().replaceAll("\\s+", "").substring(5, 9);
         int intFirstPriceInCompare = Integer.parseInt(firstPriceInCompare);
