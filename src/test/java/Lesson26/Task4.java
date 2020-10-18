07fa41278292c84520803fc454a9852d3114358f
k.shkvarun <k0styan19894@gmail.com>
2020-10-19 00:53:03 +0300

    Add lesson 25

--- src/test/java/Lesson26/Task4.java
+++ src/test/java/Lesson26/Task4.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static java.lang.Thread.sleep;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.AssertJUnit.assertTrue;
 
 public class Task4 extends BaseClass {
@@ -19,22 +22,28 @@ public class Task4 extends BaseClass {
 
     @Test
 
-    public void Task1() {
-        WebElement From1 = driver.findElement(By.xpath("//*[@id='credit2']/a"));
-        WebElement To1 = driver.findElement(By.xpath("//*[@id='bank']/li"));
-        WebElement From2 = driver.findElement(By.xpath("//*[@id='credit1']/a"));
-        WebElement To2 = driver.findElement(By.xpath("//*[@id='loan']/li"));
-        WebElement From3 = driver.findElement(By.xpath("//*[@id='fourth']/a"));
-        WebElement To3 = driver.findElement(By.xpath("//*[@id='amt7']/li"));
-        WebElement From4 = driver.findElement(By.xpath("//*[@id='fourth']/a"));
-        WebElement To4 = driver.findElement(By.xpath("//*[@id='amt8']/li"));
+    public void Task1() throws InterruptedException {
 
         Actions act = new Actions(driver);
-        act.dragAndDrop(From1, To1).build().perform();
-        act.dragAndDrop(From2, To2).build().perform();
-        act.dragAndDrop(From3, To3).build().perform();
-        act.dragAndDrop(From4, To4).build().perform();
 
-        assertTrue(driver.findElement(By.cssSelector("div.table4_result>a.button.button-green")).isDisplayed());
+        WebElement bank  = driver.findElement(By.xpath("//*[@id='credit2']/a"));
+        WebElement debitAccount = driver.findElement(By.xpath("//*[@id='bank']/li"));
+
+        WebElement sales = driver.findElement(By.xpath("//*[@id='credit1']/a"));
+        WebElement debitAmount = driver.findElement(By.xpath("//*[@id='amt7']/li"));
+
+        WebElement debMoney = driver.findElement(By.xpath("//li[@id='fourth']"));
+        WebElement creditAccount = driver.findElement(By.xpath("//*[@id='loan']/li"));
+
+        WebElement credMoney = driver.findElement(By.xpath("//li[@id='fourth']"));
+        WebElement creditAmount = driver.findElement(By.xpath("//*[@id='amt8']/li"));
+
+
+        act.dragAndDrop(bank, debitAccount).build().perform();
+        act.dragAndDrop(debMoney, debitAmount).build().perform();
+        act.dragAndDrop(sales, creditAccount).build().perform();
+        act.dragAndDrop(credMoney, creditAmount).build().perform();
+
+        assertThat(driver.findElement(By.cssSelector("a[class='button button-green']")).getText(), equalTo("Perfect!"));
     }
 }
